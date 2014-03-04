c922d07049922c0ff87feb3b86f9b7029187c448
Mike Lim <mike@mjlim.net>
2014-03-03 18:58:27 -0800

    initial hacking

---
+++ pkmn.py
@@ -0,0 +1,31 @@
+from __future__ import division
+import numpy as np
+import cv2
+
+
+GBARES = (240,160)
+
+img = cv2.imread('usepursuit.png', 1)
+
+print img.shape # (y, x, ?)
+scaling_factor = (img.shape[1] / GBARES[0], img.shape[0] / GBARES[1])
+print "scaling factor:", scaling_factor
+
+template = cv2.imread('char-P.png', 1)
+# resize template to match
+template_oldsize = template.shape
+template_newsize = (template_oldsize[1]*3, template_oldsize[0]*3)
+template = cv2.resize(template, template_newsize)
+
+result = cv2.matchTemplate(img, template, cv2.TM_SQDIFF)
+
+print result
+matchpos = np.unravel_index(np.argmax(result), result.shape)
+print matchpos
+matchpos = (matchpos[1], matchpos[0])
+
+cv2.rectangle(img, matchpos, (0,0), -1)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
