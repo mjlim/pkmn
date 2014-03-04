39dcf9d0686576f679ea1a2182fc5ef85ec2865a
Mike Lim <mike@mjlim.net>
2014-03-04 01:34:03 -0800

    YEAH

--- pkmn.py
+++ pkmn.py
@@ -3,123 +3,113 @@ import numpy as np
 import cv2
 import sys
 import os, os.path
-#import SimpleCV
 
 CHARWINWIDTH = 7
 CHARWINMOVE = 5
 
-class scalehelper:
-    def __init__(self,native, scaled):
-        self.native = native
-        self.scaled = scaled
-        self.scaling_factor = (scaled[0] / GBARES[0], scaled[1] / GBARES[1])
-        print "SF:", self.scaling_factor
-    def scalecoords(self,coords):
-        return (int(self.scaling_factor[0] * coords[0]), int(self.scaling_factor[1] * coords[1]))
-    def scalerect(self,rect):
-        return (self.scalecoords(rect[0]), self.scalecoords(rect[1]))
-    def scalex(self,num):
-        return int(self.scaling_factor[0] * num)
-
-def id_letter(img):
-    #identify best letter match in a region
-    global letters
-
-    smallest = "*"
-    smallestVal = -1 #arbitrarily large. TODO: do something smarter
-    smallestwidth = -1 # needed to advance cursor
-    for l in letters:
-        letterstr = l[0]
-        letterimg = l[1]
-        result = cv2.matchTemplate(img, letterimg, cv2.TM_SQDIFF)
-        minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(result)
-
-        if minVal < smallestVal or smallestVal == -1:
-            smallestVal=minVal
-            smallest = letterstr
-            smallestwidth = letterimg.shape[1]
-
-    cv2.rectangle(img, minLoc, (0,0), -1)
-
-    if smallest not in [' ', ''] and False:
-        cv2.imshow('image',img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    #if smallestVal > 7000000:
-        #return ('', 2)
-    return (smallest, smallestwidth)
-
-def read_line(imgline):
-    global CHARWINWIDTH
-
-    lineheight = imgline.shape[0]
-    linewidth = imgline.shape[1]
-
-    pos = 0
-    chars = []
-    while pos < linewidth - CHARWINWIDTH:
-        letter = id_letter(cropimg(imgline,((max(pos-3,0),0),(pos + CHARWINWIDTH, lineheight))))
-        chars.append(letter[0])
-        #print "".join(chars)
-        pos += letter[1]
-
-    return chars
-
-
-
-
-def cropimg(img,bounds):
-    return img[bounds[0][1]:bounds[1][1], bounds[0][0]:bounds[1][0]]
-
 GBARES = (240,160)
 
-img = cv2.imread(sys.argv[1], 1)
-img = cv2.resize(img, GBARES)
+class pkmnimage:
+    overridechars = {'period': '.', 'questionmark': '?', 'space': ' ', 'exclamationmark': '!', 'apostrophe':"'", 'hyphen': '-'}
+    def __init__(self, image):
+        self.img = cv2.resize(image, GBARES)
+        self.letters = {}
+        self.initletters()
+        self.showeveryletter = False # debug flag
+        self.charwinwidth = {'battletext': 7, 'movetext': 5}
 
-letters = []
-for limg in os.listdir('letters/battletext'):
-    letterimg = cv2.imread('letters/battletext/' + limg, 1)
+    def initletters(self):
+        for lettermap in os.listdir('letters/'):
+            self.letters[lettermap] = []
+            for limg in os.listdir('letters/' + lettermap + '/'):
+                letterimg = cv2.imread('letters/' + lettermap + '/' + limg, 1)
+                # TODO: this is bad, fix it.
+                if limg[:3] == 'cap':
+                    self.letters[lettermap].append((limg[3:-4], letterimg))
+                elif limg[:-4] in self.overridechars:
+                    self.letters[lettermap].append((self.overridechars[limg[:-4]], letterimg))
+                else:
+                    self.letters[lettermap].append((limg[:-4], letterimg))
 
-    # TODO: this is bad, fix it
-    if limg[:3] == 'cap':
-        letters.append((limg[3:-4], letterimg))
-    elif limg[:-4] == 'space':
-        letters.append((' ', letterimg))
-    else:
-        letters.append((limg[:-4], letterimg))
+    def get_text_in_region(self, bounds, lettermap):
+        line = self.read_line(self.cropimg(self.img, bounds), lettermap)
+        return "".join(line)
 
+
+
+    def id_letter(self,img,lettermap):
+        #identify best letter match in a region
+
+        smallest = "*"
+        smallestVal = -1 #arbitrarily large. TODO: do something smarter
+        smallestwidth = -1 # needed to advance cursor
+        for l in self.letters[lettermap]:
+            letterstr = l[0]
+            letterimg = l[1]
+            result = cv2.matchTemplate(img, letterimg, cv2.TM_SQDIFF)
+            minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(result)
+
+            if minVal < smallestVal or smallestVal == -1:
+                smallestVal=minVal
+                smallest = letterstr
+                smallestwidth = letterimg.shape[1]
+
+        # mark up image with found stuff
+        #cv2.rectangle(img, minLoc, (0,0), -1)
+
+        if smallest not in [' ', ''] and self.showeveryletter:
+            cv2.imshow('image',img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+        #if smallestVal > 7000000:
+            #return ('', 2)
+        return (smallest, smallestwidth)
+
+    def read_line(self, imgline, lettermap):
+        lineheight = imgline.shape[0]
+        linewidth = imgline.shape[1]
+
+        pos = 0
+        chars = []
+        while pos < linewidth - self.charwinwidth[lettermap]:
+            # magic?
+            #letter = self.id_letter(self.cropimg(imgline,((max(pos-3,0),0),(pos + CHARWINWIDTH, lineheight))), lettermap)
+            letter = self.id_letter(self.cropimg(imgline,((pos,0),(pos + self.charwinwidth[lettermap], lineheight))), lettermap)
+            chars.append(letter[0])
+            #print "".join(chars)
+            pos += letter[1]
+
+        return chars
+    def cropimg(self,img,bounds):
+        return img[bounds[0][1]:bounds[1][1], bounds[0][0]:bounds[1][0]]
+
+    def get_battletext(self):
+        # return generic text displayed in battle.
+        line1_bounds = ((10,122),(230,122+16))
+        line2_bounds = ((10,138),(230,138+16))
+        line1 = self.get_text_in_region(line1_bounds, 'battletext')
+        line2 = self.get_text_in_region(line2_bounds, 'battletext')
+        return [line1,line2]
+
+    def get_movetext(self):
+        # return all moves in the move selection screen
+        move_bounds = [((16,125),(80,133)), ((88,125),(152,133)), ((16,141),(80,149)), ((88,141),(152,149))]
+        moves = [pkimg.get_text_in_region(bound, 'movetext') for bound in move_bounds]
+        return moves
+
+
+
+
+pkimg = pkmnimage(cv2.imread(sys.argv[1], 1))
 
 #textbox_bounds = ((8,120),(230,152))
 #textbox_bounds = ((8,123),(230,138))
-textbox_bounds = ((10,122),(230,122+16))
 
-line2_bounds = ((10,138),(230,138+16))
+print "BATTLE TEXT:", pkimg.get_battletext()
+print "MOVES:", pkimg.get_movetext()
 
-
-#template = 
-# resize template to match
-#template_oldsize = template.shape
-#template_newsize = (template_oldsize[1]*3, template_oldsize[0]*3)
-#template = cv2.resize(template, template_newsize)
-#
-#result = cv2.matchTemplate(img, template, cv2.TM_SQDIFF)
-
-#matchpos = np.unravel_index(np.argmax(result), result.shape)
-#print matchpos
-#matchpos = (matchpos[1], matchpos[0])
-
-#minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(result)
-
-#print minLoc
-
-#cv2.rectangle(img, minLoc, (0,0), -1)
-
-print "".join(read_line(cropimg(img, textbox_bounds)))
-print "".join(read_line(cropimg(img, line2_bounds)))
-#for i in letters:
-    #cv2.imshow('template'+i[0],i[1])
-cv2.imshow('image',img)
+#cv2.imshow('image',pkimg.img)
 #cv2.imshow('image',cropimg(img,l2_tb_bounds))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
