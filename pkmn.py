2f3145639b9e73a5bc9c16a755fc483b0544a76e
Mike Lim <mike@mjlim.net>
2014-03-11 23:08:17 -0700

    more intelligently pick letters out of the image to compare

--- pkmn.py
+++ pkmn.py
@@ -3,9 +3,11 @@ import numpy as np
 import cv2
 import sys
 import os, os.path
+import time
 
 CHARWINWIDTH = 7
 CHARWINMOVE = 5
+SPACE_THRESHOLD = 3 # a space of 3 pixels wide or longer without any text pixels detected is considered a space
 
 GBARES = (240,160)
 
@@ -17,6 +19,7 @@ class pkmnimage:
         self.initletters()
         self.showeveryletter = False # debug flag
         self.charwinwidth = {'battletext': 7, 'movetext': 5}
+        self.chartextcolor = {'battletext': np.array([255,255,255]), 'movetext': np.array([74,73,74])}
 
     def initletters(self):
         for lettermap in os.listdir('letters/'):
@@ -32,6 +35,7 @@ class pkmnimage:
                     self.letters[lettermap].append((limg[:-4], letterimg))
 
     def get_text_in_region(self, bounds, lettermap):
+        #line = self.new_read_line(self.cropimg(self.img, bounds), lettermap, self.chartextcolor[lettermap])
         line = self.read_line(self.cropimg(self.img, bounds), lettermap)
         return "".join(line)
 
@@ -46,6 +50,10 @@ class pkmnimage:
         for l in self.letters[lettermap]:
             letterstr = l[0]
             letterimg = l[1]
+
+            # don't try to match if the letter is bigger than our region.
+            if letterimg.shape[0] > img.shape[0] or letterimg.shape[1] != img.shape[1]:
+                continue
             result = cv2.matchTemplate(img, letterimg, cv2.TM_SQDIFF)
             minVal,maxVal,minLoc,maxLoc = cv2.minMaxLoc(result)
 
@@ -66,21 +74,37 @@ class pkmnimage:
             #return ('', 2)
         return (smallest, smallestwidth)
 
-    def read_line(self, imgline, lettermap):
+    def read_line(self, imgline, lettermap, textcolor):
         lineheight = imgline.shape[0]
         linewidth = imgline.shape[1]
+        left = 0
+        line = []
+        space = 0
+        prev_num_txpx = -1
+        for right in range(1,linewidth):
+            # scan this column for text color pixels.
+            num_txpx = 0 # number of text colored pixels found
+            for y in range(0,lineheight):
+                if (imgline[y][right] == textcolor).all():
+                    num_txpx += 1
+            if num_txpx == 0:
+                if prev_num_txpx > 0: # previous column had some text pixels in it, so this is column terminates that character
+                    if space > SPACE_THRESHOLD:
+                        line.append(' ')
+                    space = 0
 
-        pos = 0
-        chars = []
-        while pos < linewidth - self.charwinwidth[lettermap]:
-            # magic?
-            #letter = self.id_letter(self.cropimg(imgline,((max(pos-3,0),0),(pos + CHARWINWIDTH, lineheight))), lettermap)
-            letter = self.id_letter(self.cropimg(imgline,((pos,0),(pos + self.charwinwidth[lettermap], lineheight))), lettermap)
-            chars.append(letter[0])
-            #print "".join(chars)
-            pos += letter[1]
+                    candidate = self.cropimg(imgline,((left,0),(right+1,lineheight)))
+                    #cv2.imshow('candidate', candidate)
+                    #cv2.waitKey(0)
+                    letter = self.id_letter(candidate, lettermap)
+                    line.append(letter[0])
+                else: # the previous column was also blank, so we're in a space
+                    space += 1
+                left = right + 1
+            prev_num_txpx = num_txpx
+        return line
 
-        return chars
+
     def cropimg(self,img,bounds):
         return img[bounds[0][1]:bounds[1][1], bounds[0][0]:bounds[1][0]]
 
@@ -102,15 +126,18 @@ class pkmnimage:
 
 
 if __name__ == '__main__':
-	pkimg = pkmnimage(cv2.imread(sys.argv[1], 1))
+    pkimg = pkmnimage(cv2.imread(sys.argv[1], 1))
 
-	#textbox_bounds = ((8,120),(230,152))
-	#textbox_bounds = ((8,123),(230,138))
+    #textbox_bounds = ((8,120),(230,152))
+    #textbox_bounds = ((8,123),(230,138))
 
-	print "BATTLE TEXT:", pkimg.get_battletext()
-	print "MOVES:", pkimg.get_movetext()
+    start = time.clock()
+    print "BATTLE TEXT:", pkimg.get_battletext()
+    print "MOVES:", pkimg.get_movetext()
+    end = time.clock()
+    print "%.2gs" % (end-start)
 
-	#cv2.imshow('image',pkimg.img)
-	#cv2.imshow('image',cropimg(img,l2_tb_bounds))
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+    #cv2.imshow('image',pkimg.img)
+    #cv2.imshow('image',cropimg(img,l2_tb_bounds))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
